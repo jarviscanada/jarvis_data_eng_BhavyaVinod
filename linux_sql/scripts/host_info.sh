5d75d8a9301deee6a0a171251c4c69e2f10b90e1
Bhavya Vinod <bhavyavinod45@gmail.com>
2021-10-27 22:36:30 +0000

    modified host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# host_info table.
+
+# Checking for parameters
+if [ "$#" != "5" ];
+then
+  echo "Incorrect number of parameters provided."
+fi
+
+# Setting arguments
+
+psql_host=$1
+psql_port=$2
+database=$3
+psql_user=$4
+psql_password=$5
+
+# Saving host name to a variable.
+hostname=$(hostname -f)
+
+# Linux information.
+lscpu_output=`lscpu`
+meminfo=`cat /proc/meminfo`
+
+# Saving hardware details into new variables
+cpu_number=$(echo "$lscpu_output" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_output" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_output" | egrep "^Model name:" | awk -F':' '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_output" | egrep "^CPU MHz:" | awk -F':' '{print $2}' | xargs)
+l2_cache=$(echo "$lscpu_output" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(echo "$meminfo" | egrep "MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(vmstat -t | awk '{if(NR==3) print $18" "$19}' | xargs)
+
+export PGPASSWORD=$psql_password
+psql -h $hostname -U $psql_user -w $database_name -p $psql_port -c \
+	"INSERT INTO host_info
+	 (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) 
+	 VALUES ('"$hostname"', '"$cpu_number"', '"$cpu_architecture"', '"$cpu_model"', '"$cpu_mhz"', '"$l2_cache"', '"$total_mem"', '"$timestamp"');"
+
+exit 0
+
+
