835384dac1e9c80acf9544c52f9ac879d12506b9
Bhavya Vinod <bhavyavinod45@gmail.com>
2021-10-28 22:47:42 +0000

    updated info_sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# host_info table.
+psql_host=$1
+psql_port=$2
+database_name=$3
+psql_user=$4
+psql_password=$5
 
 # Checking for parameters
-if [ "$#" != "5" ];
+if [ "$#" -ne 5 ];
 then
   echo "Incorrect number of parameters provided."
 fi
-
-# Setting arguments
-
-psql_host=$1
-psql_port=$2
-database=$3
-psql_user=$4
-psql_password=$5
 
 # Saving host name to a variable.
 hostname=$(hostname -f)
@@ -26,20 +22,16 @@ lscpu_output=`lscpu`
 # Saving hardware details into new variables
 cpu_number=$(echo "$lscpu_output" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_output" | egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_output" | egrep "^Model name:" | awk -F':' '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_output" | egrep "Model name" | awk '{print $2}' | xargs)
 cpu_mhz=$(echo "$lscpu_output" | egrep "^CPU MHz:" | awk -F':' '{print $2}' | xargs)
 l2_cache=$(echo "$lscpu_output" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2 $3}'| xargs)
-
-
 timestamp=$(vmstat -t | awk '{if(NR==3) print $18" "$19}' | xargs)
 
-export PGPASSWORD=$psql_password
-psql -h $hostname -U $psql_user -w $database_name -p $psql_port -c \
-	"INSERT INTO host_info
-	 (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) 
+insert_stmt="INSERT INTO host_info
+	 (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp)
 	 VALUES ('"$hostname"', '"$cpu_number"', '"$cpu_architecture"', '"$cpu_model"', '"$cpu_mhz"', '"$l2_cache"', '"$total_mem"', '"$timestamp"');"
 
-exit 0
-
-
+export PGPASSWORD=$psql_password
+psql -h $hostname -U $psql_user -w $database_name -p $psql_port -c "$insert_stmt"
+exit 0 
--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,37 +1,28 @@
-DROP DATABASE IF EXISTS host_agent;
-CREATE DATABASE host_agent;
+--connect to host agent
+\c host_agent;
 
-\c host_agent
+--Creates host_info table only if it does not exists to store hardware specifications
+CREATE TABLE iF NOT EXISTS public.host_info
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    hostname VARCHAR NOT NULL UNIQUE,
+    cpu_number INT NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model INT NOT NULL,
+    cpu_mhz  INT NOT NULL,
+    L2_cache VARCHAR NOT NULL,
+    total_mem INT NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL
+);
 
-DROP TABLE IF EXISTS PUBLIC.host_info;
-DROP TABLE IF EXISTS PUBLIC.host_usage;
-
-CREATE TABLE PUBLIC.host_info
-    (
-        id			SERIAL NOT NULL UNIQUE,
-        hostname		VARCHAR NOT NULL UNIQUE,
-	cpu_number		INT2 NOT NULL,
-	cpu_architecture	VARCHAR NOT NULL,
-	cpu_model		VARCHAR NOT NULL,
-	cpu_mhz			FLOAT8 NOT NULL,
-	l2_cache		INT4 NOT NULL,
-	total_mem		INT4 NOT NULL,
-	timestamp		TIMESTAMP NOT NULL,
-	PRIMARY KEY (id, hostname)
-    );
-
-CREATE TABLE PUBLIC.host_usage
-    (
-	id		SERIAL NOT NULL,
-	hostname 	VARCHAR NOT NULL REFERENCES host_info(hostname),
-	memory_free	INT4 NOT NULL,
-	cpu_idle	INT2 NOT NULL,
-	cpu_kernel	INT2 NOT NULL,
-	disk_io		INT2 NOT NULL,
-	disk_available	INT4 NOT NULL,
-	timestamp	TIMESTAMP NOT NULL,
-	PRIMARY KEY (id, timestamp),
-	FOREIGN KEY (id) REFERENCES host_info(id)
-    );
-
-
+--Creates host_usage table only if it does not exist to store resource usage data
+CREATE TABLE if NOT EXISTS PUBLIC.host_usage
+(
+    "timestamp" TIMESTAMP NOT NULL,
+    host_id SERIAL NOT NULL REFERENCES host_info(id),
+    memory_free INT NOT NULL,
+    cpu_idle INT NOT NULL,
+    cpu_kernel INT NOT NULL,
+    disk_io INT NOT NULL,
+    disk_available INT NOT NULL
+);
